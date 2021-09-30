fd673af07760b2c9d31f23c4c348dc8f9f2b3665
Johnny952 <jonathan.espinola@ug.uchile.cl>
2021-09-30 15:07:03 -0300

    add docker compose

--- get_batches.sh
+++ get_batches.sh
@@ -3,5 +3,5 @@
 for i in `seq 1 100`
 do
     echo $i
-    python data_collect.py -p 2004 -T 1000 --skip-frames 0 -B $i
+    python data_collect.py -H carla -p 2004 -T 1000 --skip-frames 0 -B $i
 done
